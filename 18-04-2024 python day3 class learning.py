2692a9ef0a1d41f4a30dea8107a4844cb277359d
sagarsambhwani <165025855+sagarsambhwani@users.noreply.github.com>
2024-04-18 10:50:22 +0530

    Add files via upload

---
+++ 18-04-2024 python day3 class learning.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+state ="rajasthan"
+state[5]
+
+
+# In[2]:
+
+
+state[-5]
+
+
+# In[3]:
+
+
+len(state)
+
+
+# In[4]:
+
+
+state[0:4]
+
+
+# In[5]:
+
+
+state[0:6:2]
+
+
+# In[6]:
+
+
+state[0:]
+
+
+# In[7]:
+
+
+state[:9]
+
+
+# In[8]:
+
+
+state[-4::-1]
+
+
+# In[9]:
+
+
+state[-9::1]
+
+
+# In[10]:
+
+
+x=15
+if(x==14):
+    print("heudw")
+else:
+    print("hywhd")
+
+
+# In[11]:
+
+
+x=1
+y=2
+z=3
+if(x<y and x<z):
+    print("x is smallest")
+elif(y<z and y<x):
+    print("y is smallest")
+else:
+    print("z is smallest")
+
+
+# In[12]:
+
+
+x=input("enter the number: ")
+
+
+# In[13]:
+
+
+x=int(input("enter the number: "))
+
+if(x%2==0):
+    print("number is even")
+else:
+    print("number is odd")
+
+
+# In[14]:
+
+
+x=int(input("enter the number: "))
+
+if(x%2==0 and x%3==0):
+    print("number is divisible")
+else:
+    print("number is not divisible")
+
+
+# In[ ]:
+
+
+
+
